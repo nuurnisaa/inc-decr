4f87f8783b946e72ddaa68f0aec33bec30664b75
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-08-25 00:15:12 +0800

    Add files via upload

---
+++ increment decrement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1=30;
+    
+   
+    cout<<num1++<<endl; //post inc
+    cout<<num1<<endl;
+    cout<<++num1<<endl; //pre inc
+    cout<<num1+1<<endl;
+   
+    cout<<num1--<<endl;
+    cout<<num1<<endl;
+    cout<<--num1<<endl;
+    cout<<num1-1<<endl;
+  
+    
+    
+    
+    return 0;
+}
